1205b7edadfb07d6c9a1885aa6b882003c13e04d
GrigoriyM1 <alek9051@gmail.com>
2024-12-23 17:49:36 +0400

    sdsdsd

--- app.constants.ts
+++ app.constants.ts
@@ -1,6 +1,7 @@
-export const APP_WRITE_ID = process.env.APPWRITE_ID!
-export const DB_ID = process.env.APPWRITE_DB_ID!
-export const COLLECTION_DEALS = process.env.APPWRITE_COLLECTION_DEALS!
-export const COLLECTION_CUSTOMERS = process.env.APPWRITE_COLLECTION_CUSTOMERS!
-export const COLLECTION_COMMENTS = process.env.APPWRITE_COLLECTION_COMMENTS!
-export const STORAGE_ID = process.env.APPWRITE_STORAGE_ID!
+export const APP_WRITE_ID = 'vue-crm-test' // айди который в коде при создании дается appwrite 
+export const DB_ID = 'vue-crm-db'
+export const COLLECTION_DEALS = 'deals' // create collections  "deals" ит д 
+export const COLLECTION_CUSTOMERS = 'customers'
+export const COLLECTION_COMMENTS = 'comments'
+export const STORAGE_ID = 'storage' // https://cloud.appwrite.io/console/project-vue-crm-test/storage 
+// можно и в env вынести 
